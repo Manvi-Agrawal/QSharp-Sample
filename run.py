008512b0235ed6aaeed0edf8bc9617ae43c80521
Manvi-Agrawal <40084144+Manvi-Agrawal@users.noreply.github.com>
2024-04-12 10:52:21 -0700

    Create README.md

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # Following https://learn.microsoft.com/en-us/azure/quantum/user-guide/how-to-work-with-qsharp-projects?tabs=tabid-qsharp%2Ctabid-python-run#steps-for-creating-a-q-project
 import qsharp
 # set the root folder for the project
-qsharp.init(project_root = '/Teleportation_project')
+qsharp.init(project_root = '\Teleportation_project')
 
 # Getting error
